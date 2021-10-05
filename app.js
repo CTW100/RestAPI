dca743d32b044258eeebe9d24104e8ae3c720b64
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-06 02:55:20 +0900

    365
    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded : this is the default data that data has if submitted through a form post request
 app.use(bodyParser.json()); // application/json &&&& able to parse json data from incoming request because we expect to communicate with our user in json data form
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
