cc885e24671667dc339e508020d833d14fdf7191
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-06 04:09:14 +0900

    373
    Fetching Lists of Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,18 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     // allows as to conveniently return a response with json data, with the right headers being set and so on
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/Steve.jpg',
+        creator: {
+          name: 'Lee',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
