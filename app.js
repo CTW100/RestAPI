7ee76bae2c6a56e567e43c19a0d1f062500de8cc
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-06 02:38:10 +0900

    364
    Sending Requets & Responses and Working with Postman

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = reuqire('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded : this is the default data that data has if submitted through a form post request
+app.use(bodyParser.json()); // application/json &&&& able to parse json data from incoming request because we expect to communicate with our user in json data form
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,16 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    // allows as to conveniently return a response with json data, with the right headers being set and so on
+    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post crated successfully!',
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
