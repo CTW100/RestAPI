89f8875c0c154f2c7c9659d9736aa85ba9ce9d4f
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-07 01:45:55 +0900

    377
    Storing Posts in the Database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     // allows as to conveniently return a response with json data, with the right headers being set and so on
@@ -21,24 +23,29 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post crated successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      createor: { name: 'Lee' },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: '/images/Steve.jpg',
+    createor: { name: 'Lee' },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post crated successfully!',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
