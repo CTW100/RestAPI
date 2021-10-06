03a326819bb92b850376dcdf26556c94d8ec7cb7
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-07 01:39:32 +0900

    376
    Setting Up a Post Model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -19,4 +20,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://sunbelt:sohkaniz65@cluster0.c0d1i.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
