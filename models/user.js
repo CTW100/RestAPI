600240074c32f7952603931c43f7a1ca05d5447b
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-08 18:55:08 +0900

    385
    Adding a User Model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  oists: [
+    {
+      type: Schema.Types.ObjectId, // this will be a reference to a post
+      ref: 'Post', // 'Post' : it indicates post model
+    },
+  ],
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+// we can now use user model to set up a sign up and later, also a login route
